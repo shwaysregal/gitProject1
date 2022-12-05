d7d3225fb09f94fb477acd8946bb2aaf58ba346f
shwaysregal <hasanberkaycelikkaya@gmail.com>
2022-12-05 12:47:37 +0300

    Young Lads Good lads

--- src/Work1/goodMorninNam.java
+++ src/Work1/goodMorninNam.java
@@ -4,5 +4,6 @@ public class goodMorninNam {
     public static void main(String[] args) {
         System.out.println("Eagle screams!!");
         System.out.println("Many young lads died there god bless them souls");
+        System.out.println("nam movies are so good");
     }
 }
