f5ce2be3a41f9f201e6ceea9d3617eb032ab5caf
shwaysregal <hasanberkaycelikkaya@gmail.com>
2022-12-08 21:52:04 +0300

    sixties hippies

--- src/Work1/hippiesSixties.java
+++ src/Work1/hippiesSixties.java
@@ -3,6 +3,7 @@ package Work1;
 public class hippiesSixties {
     public static void main(String[] args) {
         System.out.println("They rode harleys maybe");
+        System.out.println("dont take anything they give you");
 
 
     }
