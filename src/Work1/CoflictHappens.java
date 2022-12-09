c208c19dda874e5cd1d0b3b2336871eda2e60018
shwaysregal <hasanberkaycelikkaya@gmail.com>
2022-12-09 19:26:20 +0300

    Conflict boutta happen

---
+++ src/Work1/CoflictHappens.java
@@ -0,0 +1,8 @@
+package Work1;
+
+public class CoflictHappens {
+    public static void main(String[] args) {
+        System.out.println("i felt like killing rockn roll");
+        System.out.println("yeah");
+    }
+}
